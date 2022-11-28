ffd1f303b8af86399c9134448a21e4a5663ded96
Sam Llanwarne <errerr46@gmail.com>
2022-11-28 18:14:27 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -55,7 +55,7 @@ def linearRegression(x_train, y_train, x_test=None, y_test=None):
 ## 2/14: Logistic Regression
 
 def logRegression(x_train, y_train, x_test=None, y_test=None):
-    model = sklearn.linear_model.LogisticRegression().fit(x_train, y_train)
+    model = sklearn.linear_model.LogisticRegression(max_iter=10000).fit(x_train, y_train)
     if type(x_test) != type(None):
         predictions = model.predict(x_test)
         if type(y_test) != type(None):
@@ -67,8 +67,9 @@ def logRegression(x_train, y_train, x_test=None, y_test=None):
 
 ## 3/14: MLP
 
+from sklearn.neural_network import MLPClassifier as MLP
 def MLPclassifier(x_train, y_train, x_test=None, y_test=None):
-    model = sklearn.neural_network.MLPClassifier()
+    model = MLP()
     model.fit(x_train, y_train)
     if type(y_test) != type(None):
         predictions = model.predict(y_test)
